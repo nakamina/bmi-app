6ceb76ff84dad07010c2742ddce0818e965ae71d
nakamina <n.mina10.0608gmail.com>
2023-03-21 00:38:45 +0900

    fix calculation of proper_weight

--- bmi_app.py
+++ bmi_app.py
@@ -2,14 +2,14 @@
 def calculate_bmi(height, weight):
     # 身長と体重からBMIを計算する関数
     height_m = height / 100
-
-    bmi = weight / (height_m**2)
+    bmi = weight / height_m**2
     return bmi
 
 
 def calculate_proper_weight(height):
     # 身長から適正体重を計算する関数
-    proper_weight = (height - 100) * 0.9
+    height_m = height / 100
+    proper_weight = (height_m**2) * 22
     return proper_weight
 
 
@@ -17,22 +17,28 @@ if __name__ == "__main__":
     # 身長と体重を入力する
 
     while True:
-        height_str = input("身長(cm)を入力してください：")
+        height_str = input("身長(cm)を入力してください:")
         try:
             height = float(height_str)
         except ValueError:
-            print("正しい入力ではありません。再度入力してください。")
+            print("正しい入力ではありません。再度入力してください")
             continue
 
         if height > 0:
             break
-        print("正しい入力ではありません。再度入力してください。")
+        print("正しい入力ではありません。再度力してください")
 
     while True:
-        weight = float(input("体重(kg)を入力してください："))
+        weight_str = input("体重(kg)を入力してください:")
+        try:
+            weight = float(weight_str)
+        except ValueError:
+            print("正しい入力ではありません。再度入力してください")
+            continue
+
         if weight > 0:
             break
-        print("正しい入力ではありません。再度入力してください。")
+        print("正しい入力ではありません。再度力してください")
 
     # BMIと適正体重を計算する
     bmi = calculate_bmi(height, weight)
