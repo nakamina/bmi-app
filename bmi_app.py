675220f26ff8e2250acf3cd3de4ac475ae953fa6
nakamina <n.mina10.0608gmail.com>
2023-05-09 22:10:28 +0900

    update bmi_app.py

--- bmi_app.py
+++ bmi_app.py
@@ -1,45 +1,25 @@
+import click
+
+
 # BMI計算アプリケーション
-def calculate_bmi(height, weight):
+def calculate_bmi(h: int, w: int) -> float:
     # 身長と体重からBMIを計算する関数
-    height_m = height / 100
-    bmi = weight / height_m**2
+    height_m = h / 100
+    bmi = w / height_m**2
     return bmi
 
 
-def calculate_proper_weight(height):
+def calculate_proper_weight(h: int) -> float:
     # 身長から適正体重を計算する関数
-    height_m = height / 100
+    height_m = h / 100
     proper_weight = (height_m**2) * 22
     return proper_weight
 
 
-if __name__ == "__main__":
-    # 身長と体重を入力する
-
-    while True:
-        height_str = input("身長(cm)を入力してください:")
-        try:
-            height = float(height_str)
-        except ValueError:
-            print("正しい入力ではありません。再度入力してください")
-            continue
-
-        if height > 0:
-            break
-        print("正しい入力ではありません。再度力してください")
-
-    while True:
-        weight_str = input("体重(kg)を入力してください:")
-        try:
-            weight = float(weight_str)
-        except ValueError:
-            print("正しい入力ではありません。再度入力してください")
-            continue
-
-        if weight > 0:
-            break
-        print("正しい入力ではありません。再度力してください")
-
+@click.command()
+@click.option("--height", type=int, required=True, help="身長(cm)")
+@click.option("--weight", type=int, required=True, help="体重(kg)")
+def main(height: int, weight: int) -> None:
     # BMIと適正体重を計算する
     bmi = calculate_bmi(height, weight)
     proper_weight = calculate_proper_weight(height)
